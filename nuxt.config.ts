94fb96db2db80ab3f4fe0694e54aa6b56d482d54
zRakan <122341529+zRakan@users.noreply.github.com>
2023-11-25 18:58:30 +0300

    SEO meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,35 @@ export default defineNuxtConfig({
         lang: 'ar'
       },
 
-      title: "Links | Shelf team",
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" }, // Viewport
+
+        // SEO properties
+          // Google
+          { name: "description", content: "اهم الروابط في جامعة الإمام محمد بن سعود الإسلامية، كلية علوم الحاسب والمعلومات، في مكان واحد" },
+
+          // Twitter
+          { name: "twitter:card", content: "summary_large_image" },
+        
+          // Status bar customization
+          { name: "theme-color", content: "#040303" },
+          { name: "msapplication-navbutton-color", content: "#040303" },
+          { name: "apple-mobile-web-app-capable", content: "yes" }, // Sets Full-scren
+          { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" }, // Sets the style of the status bar
+
+          // Open-graph
+          { property: "og:type", content: "website" },
+          { property: "og:title", content: "Shelf Team | اهم الروابط في مكان واحد" },
+          { property: "og:url", content: "https://shelf.team/"},
+          { property: "og:image", content: "https://shelf.team/logo.webp" },
+          { property: "og:description", content: "اهم الروابط في جامعة الإمام محمد بن سعود الإسلامية، كلية علوم الحاسب والمعلومات، في مكان واحد" },
+          { property: "og:locale", content: "ar_SA" },
+          { property: "og:locale:alternate", content: "en_US" },
+
+        { charset: 'utf-8' } // Charset
+      ],
+
+      title: "Shelf Team | اهم الروابط في مكان واحد"
     }
   }
 })
